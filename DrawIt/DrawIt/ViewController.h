b15377d9c15379fcbe2473ef9233c4e5f7d3d435
ReckyLin <ReckyLin@ReckyLin-Nautilus.local>
2016-10-24 11:41:55 +0800

    init commit

---
+++ DrawIt/DrawIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawIt
+//
+//  Created by ReckyLin on 2016/10/24.
+//  Copyright © 2016年 ywl-ios1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DrawIt/DrawIt/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DrawIt
+//
+//  Created by ReckyLin on 2016/10/24.
+//  Copyright © 2016年 ywl-ios1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
